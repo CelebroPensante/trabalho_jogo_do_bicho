bee053e18ea95c40355567855895f12173d978c7
CelebroPensante <vitor.gustavo.jc@gmail.com>
2024-10-19 23:58:27 -0300

    docker

--- trabalho 4/app/data/get_data.py
+++ trabalho 4/app/data/get_data.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 def get_data():
     data_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    with open(data_path + '\\data\\_chat.txt', 'r', encoding='utf-8') as file:
+    file_path = os.path.join(data_path, 'data', '_chat.txt')
+    with open(file_path, 'r', encoding='utf-8') as file:
         data = file.read()
         
         date = re.findall(r'\d{2}/\d{2}/\d{4}', data)
